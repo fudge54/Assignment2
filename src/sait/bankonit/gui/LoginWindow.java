107b97b9d2e14231058a2036b9397be9c94f0dc3
w-Andrew <99445523+w-Andrew@users.noreply.github.com>
2022-06-26 12:05:40 -0600

    Update LoginWindow.java

--- src/sait/bankonit/gui/LoginWindow.java
+++ src/sait/bankonit/gui/LoginWindow.java
@@ -5,28 +5,26 @@ import java.awt.event.*;
 
 import javax.swing.*;
 
-import ca.bankonit.manager.*;
-import ca.bankonit.models.*;
-
 /**
  * Renders the login window
+ * 
  * @author Nick Hamnett
  * @version Aug 3, 2021
  */
 public class LoginWindow extends JFrame {
-	
+
 	/**
 	 * Initializes the login window.
 	 */
 	public LoginWindow() {
 		super("Bank On It Login");
-		
+		// test hey daniel
 		// Set window size to 500x150
 		this.setSize(500, 150);
-		
+
 		// Cause process to exit when X is clicked.
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
+
 		// Center login window in screen
 		this.setLocationRelativeTo(null);
 	}
