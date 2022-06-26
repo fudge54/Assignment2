3e077b46ec9f7f38e8ba30fa2bc44f4b2dd3559a
w-Andrew <99445523+w-Andrew@users.noreply.github.com>
2022-06-26 12:26:35 -0600

    jrefix

--- src/sait/bankonit/gui/AccountWindow.java
+++ src/sait/bankonit/gui/AccountWindow.java
@@ -1,34 +1,35 @@
 package sait.bankonit.gui;
 
-import javax.swing.*;
 import java.awt.*;
 
-import ca.bankonit.exceptions.*;
-import ca.bankonit.manager.*;
-import ca.bankonit.models.*;
+import javax.swing.*;
+
+import ca.bankonit.models.Account;
 
 /**
  * Renders the account window.
+ * 
  * @author Nick Hamnett
  * @version Aug 3, 2021
  */
 public class AccountWindow extends JFrame {
 	private Account account;
-	
+
 	/**
 	 * Initializes the account window
+	 * 
 	 * @param account Account to manage
 	 */
 	public AccountWindow(Account account) {
 		super("Bank On It Account");
-		
+
 		// Store account as field.
 		this.account = account;
-		
+
 		// Set size to 600x500
 		this.setSize(600, 500);
 	}
-	
+
 	/**
 	 * Clears and re-populates transactions as well as updates balance.
 	 */
